3c0c71615d3ea5f7e5d0a902a32b57d3776ce90b
bradthomas127 <bradthomas127@yahoo.com>
2014-01-01 01:02:27 -0500

    Finalize the TWRP-ify
    Copy the twrp.fstab to the recovery ramdisk
    
    Change-Id: I8c39f1dde79a1005079f1dd2c41163efde5bf353

--- device.mk
+++ device.mk
@@ -31,6 +31,9 @@ PRODUCT_COPY_FILES := \
     $(LOCAL_KERNEL):kernel
 
 PRODUCT_COPY_FILES += \
+    device/lge/hammerhead/twrp.fstab:recovery/root/etc/twrp.fstab
+
+PRODUCT_COPY_FILES += \
     device/lge/hammerhead/init.hammerhead.rc:root/init.hammerhead.rc \
     device/lge/hammerhead/init.hammerhead.usb.rc:root/init.hammerhead.usb.rc \
     device/lge/hammerhead/fstab.hammerhead:root/fstab.hammerhead \
